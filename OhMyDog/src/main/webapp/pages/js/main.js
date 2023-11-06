5d9535447a1b4ed14d978e3c67ca28f92182fc56
Ellen172 <ellencas172@gmail.com>
2023-11-05 21:56:07 -0300

    iniciando projeto; front do index

---
+++ OhMyDog/src/main/webapp/pages/js/main.js
@@ -0,0 +1,10 @@
+
+function addClassDivBuscaFocus(){
+	jQuery("#div-busca").addClass("div-busca-focus");
+}
+function removeClassDivBuscaFocus(){
+	jQuery("#div-busca").removeClass("div-busca-focus");
+}
+
+$(document).ready(function(){
+});
